fba4d0aeb09c0ce2606933e082fa79534f5ed1da
dalima-dev <daniel@moblive.com.br>
2022-07-12 19:33:02 -0300

    init homepage and layouts

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import Main from 'layouts/Main/Main'
+import Header from 'layouts/Header/Header'
+import Footer from 'layouts/Footer/Footer'
+
+function Home() {
+  return (
+    <>
+        <Header />
+        <Main />
+        <Footer />
+    </>
+  )
+}
+
+export default Home
